527336a4cbbd664f9c6c8e562e9dd3b08a8d0783
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2021-02-01 19:27:47 +0600

    ⬆️ Pos Inventory Rebuild Init
    Type(s): UPDATE

--- bin/remove_build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-. ./bin/parse_env.sh
-
-docker rmi registry.sheba.xyz/${CONTAINER_NAME}:latest
-docker rmi ${CONTAINER_NAME}:latest
--- bin/run-dev.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-cp -r /usr/src/cache/node_modules/. /usr/src/app/node_modules/
-npm run serve:dev
