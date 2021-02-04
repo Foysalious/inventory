6fae1874e14a4042e9c2d6afc20df05ddc7445ca
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2021-02-04 18:17:07 +0600

    ➕ Values Table
    Type(s): ADD
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-19

---
+++ database/migrations/2021_02_04_111650_create_values_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateValuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('values', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('option_id')->unsigned();
+            $table->foreign('option_id')->references('id')->on('options')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->string('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('values');
+    }
+}
