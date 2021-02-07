4454cef7321c08a90876da27286c1aab73919ac7
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2021-02-07 19:20:59 +0600

    ➕ Sku Channels Table
    Type(s): ADD
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-14

---
+++ database/migrations/2021_02_07_120201_create_sku_channels_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSkuChannelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sku_channels', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('product_id')->unsigned();
+            $table->foreign('product_id')->references('id')->on('products')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->bigInteger('channel_id')->unsigned();
+            $table->foreign('channel_id')->references('id')->on('channels')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->decimal('price', 11, 2)->unsigned();
+            commonColumns($table);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sku_channels');
+    }
+}
