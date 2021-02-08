c9eed0e3ee14f733c707fc78b757969486d56ce1
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2021-02-08 12:29:04 +0600

    ➕ Product Update Logs Table
    Type(s): ADD
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-5

---
+++ database/migrations/2021_02_08_062126_create_product_update_logs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductUpdateLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_update_logs', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('product_id')->unsigned();
+            $table->foreign('product_id')->references('id')->on('products')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->json('field_names');
+            $table->json('old_value');
+            $table->json('new_value');
+            storeColumns($table);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_update_logs');
+    }
+}
