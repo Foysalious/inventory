eb2182ede7203ecc8a9172c5d1930d1abd6f436c
shovan <shovan.sheba@gmail.com>
2021-02-16 17:54:58 +0600

    ➕ Create partner_categories
    Type(s): ADD
    
    Issue(s):
    - JIRA:
    https://blabol.atlassian.net/browse/MSBE-319?atlOrigin=eyJpIjoiNDI4MWM4NmZlYTIzNDhiMzk1MzEzNjIwMDViM2E3NDAiLCJwIjoiaiJ9

---
+++ database/migrations/2021_02_16_113905_create_partner_categories.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePartnerCategories extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('partner_categories', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('partner_id')->unsigned();
+            $table->foreign('partner_id')->references('id')->on('partners')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->bigInteger('category_id')->unsigned();
+            $table->foreign('category_id')->references('id')->on('categories')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->unique(['partner_id', 'category_id']);
+            $table->softDeletes();
+            commonColumns($table);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('partner_categories');
+    }
+}
