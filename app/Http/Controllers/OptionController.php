b5cd48c4a0884fd51e2976bc0c3548635bf78676
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2021-02-16 18:29:26 +0600

    ➕ Option Updater
    Type(s): ADD
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-242

--- app/Http/Controllers/OptionController.php
+++ app/Http/Controllers/OptionController.php
@@ -2,6 +2,7 @@
 
 use App\Http\Requests\OptionRequest;
 use App\Services\Option\OptionService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class OptionController extends Controller
@@ -14,13 +15,27 @@ class OptionController extends Controller
         $this->optionService = $optionService;
     }
 
+    /**
+     * Display a listing of the resource.
+     * @return JsonResponse
+     */
     public function index()
     {
         return $this->optionService->getAll();
     }
 
+    /**
+     * Store a newly created resource in storage.
+     * @param OptionRequest $request
+     * @return JsonResponse
+     */
     public function store(OptionRequest $request)
     {
         return $this->optionService->create($request);
+    }
+
+    public function update(OptionRequest $request, $optionId)
+    {
+        return $this->optionService->update($request, $optionId);
     }
 }
