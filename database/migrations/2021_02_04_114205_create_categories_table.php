553993c49ed0d962d2778441bb62e8b570c0528e
shovan <shovan.sheba@gmail.com>
2021-02-16 17:34:51 +0600

    ➕ Create categories table
    Type(s): ADD
    
    Issue(s):
    - JIRA:
    https://blabol.atlassian.net/browse/MSBE-209?atlOrigin=eyJpIjoiNTM0ODhiOGVlMmMzNGU3ZmJkNDJmNDQxMjlmYjIxNTkiLCJwIjoiaiJ9

--- database/migrations/2021_02_04_114205_create_categories_table.php
+++ database/migrations/2021_02_04_114205_create_categories_table.php
@@ -15,8 +15,23 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('parent_id')->nullable()->unsigned();
+            $table->foreign('parent_id')->references('id')->on('categories')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->string('name');
+            $table->string('slug');
+            $table->string('thumb')->default(getCategoryDefaultThumb())->nullable();
+            $table->string('banner')->default(getCategoryDefaultBanner())->nullable();
+            $table->string('app_thumb')->default(getCategoryDefaultThumb())->nullable();
+            $table->string('app_banner')->default(getCategoryDefaultBanner())->nullable();
+            $table->boolean('publication_status')->default(0);
+            $table->tinyInteger('is_published_for_sheba')->default(1);
+            $table->smallInteger('order')->nullable()->unsigned();
+            $table->string('icon')->nullable();
+            $table->string('icon_png')->nullable();
             commonColumns($table);
         });
+
     }
 
     /**
