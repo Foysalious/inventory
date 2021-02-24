a6b9404018e900def94253bf33cf372355e085c2
shovan <shovan.sheba@gmail.com>
2021-02-24 21:16:13 +0600

    🚧 Product discount create
    Type(s): WIP
    
    Issue(s):
    - JIRA:
    https://blabol.atlassian.net/browse/MSBE-335?atlOrigin=eyJpIjoiZWU1MzU5YmRmMGE0NGZmMGEwYzYzOTg5MDgwNTcyYWQiLCJwIjoiaiJ9

--- app/Services/Discount/Types.php
+++ app/Services/Discount/Types.php
@@ -1,10 +1,18 @@
-<?php
+<?php namespace App\Services\Discount;
 
 
-namespace App\Services\Discount;
-
+use App\Helper\ConstGetter;
 
 class Types
 {
+    use ConstGetter;
+
+    const PRODUCT = 'product';
+    const PRODUCT_CHANNEL = 'product_channel';
+    const SKU_CHANNEL = 'sku_channel';
+    const SKU = 'sku';
+    const COLLECTION = 'collection';
+    const CATEGORY = 'category';
+
 
 }
