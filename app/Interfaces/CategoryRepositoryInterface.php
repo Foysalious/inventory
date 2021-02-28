9e8c60ca88cd0dbf180ca8695874f4018b8f8192
foysal <foysal@iamsheba.xyz>
2021-02-28 17:01:34 +0600

    ➕
    Type(s): ADD
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-341

--- app/Interfaces/CategoryRepositoryInterface.php
+++ app/Interfaces/CategoryRepositoryInterface.php
@@ -3,7 +3,7 @@
 
 interface CategoryRepositoryInterface extends BaseRepositoryInterface
 {
-public function getMasterCategoriesByPartner($partner_id);
+//public function getMasterCategoriesByPartner($partner_id);
 
 public function getCategory();
 public function getCategoriesByPartner($partner_id);
