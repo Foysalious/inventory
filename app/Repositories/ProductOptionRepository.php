c2bd8b1647cc53068e42f05a54cc4a0e491825ee
shovan <shovan.sheba@gmail.com>
2021-03-02 20:15:38 +0600

    🚧 Product create with option and value
    Type(s): WIP
    
    Issue(s):
    - JIRA:
    https://blabol.atlassian.net/browse/MSBE-329?atlOrigin=eyJpIjoiOThiMmJkMWIxMmQ4NDA1YzhlYjQ1N2E2MmMwMGQ5OTYiLCJwIjoiaiJ9

---
+++ app/Repositories/ProductOptionRepository.php
@@ -0,0 +1,17 @@
+<?php namespace App\Repositories;
+
+
+use App\Interfaces\ProductOptionRepositoryInterface;
+use App\Models\ProductOption;
+
+
+class ProductOptionRepository extends BaseRepository implements ProductOptionRepositoryInterface
+{
+
+    public function __construct(ProductOption $model)
+    {
+        parent::__construct($model);
+    }
+
+
+}
