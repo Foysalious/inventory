1072f481a5bbbd7cd9354af05efe2082848277e5
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2021-03-16 18:17:51 +0600

    ⬆️ Options Values Related Table
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-401

---
+++ database/migrations/2021_03_16_120357_remove_partner_id_from_options_and_values_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RemovePartnerIdFromOptionsAndValuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('options', function (Blueprint $table) {
+            $table->dropForeign(['partner_id']);
+            $table->dropColumn('partner_id');
+        });
+        Schema::table('values', function (Blueprint $table) {
+            $table->dropForeign(['partner_id']);
+            $table->dropColumn('partner_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('options', function (Blueprint $table) {
+            $table->bigInteger('partner_id')->after('name')->nullable()->unsigned()->index();
+            $table->foreign('partner_id')->references('id')->on('partners')
+                ->onUpdate('cascade')->onDelete('set null');
+        });
+
+        Schema::table('values', function (Blueprint $table) {
+            $table->bigInteger('partner_id')->after('name')->nullable()->unsigned()->index();
+            $table->foreign('partner_id')->references('id')->on('partners')
+                ->onUpdate('cascade')->onDelete('set null');
+        });
+    }
+}
