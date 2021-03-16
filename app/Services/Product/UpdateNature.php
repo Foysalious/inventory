107c6d2834efdb64b358d137305a75f1050d7855
shovan <shovan.sheba@gmail.com>
2021-03-16 15:14:24 +0600

    wip

---
+++ app/Services/Product/UpdateNature.php
@@ -0,0 +1,14 @@
+<?php namespace App\Services\Product;
+
+use App\Helper\ConstGetter;
+
+class UpdateNature
+{
+    use ConstGetter;
+
+    const OPTION_ADD = 'option_add';
+    const OPTION_DELETE = 'option_delete';
+    const VALUE_ADD  = 'value_add';
+    const VALUE_DELETE = 'value_delete';
+
+}
