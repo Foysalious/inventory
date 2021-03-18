67fb10223b1c4a402ace6c97a758d798d0bbc23c
nafeeur <nafeeur@iamsheba.xyz>
2021-03-18 18:58:55 +0600

    ⬆️ Collection CRUD with Image
    Type(s): UPDATE

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -59,7 +59,7 @@ class CollectionController extends Controller
      */
     public function update(CollectionUpdateRequest $request, $partner_id, $collection_id)
     {
-        return $this->collectionService->update($request->validated(), $partner_id, $collection_id);
+        return $this->collectionService->update($request, $partner_id, $collection_id);
     }
 
     /**
--- app/Http/Requests/CollectionUpdateRequest.php
+++ app/Http/Requests/CollectionUpdateRequest.php
@@ -24,7 +24,8 @@ class CollectionUpdateRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => 'required|string',
+            'is_published' => 'required'
         ];
     }
 }
