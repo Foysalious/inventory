4faef3dbe09ccbcce7037f9c0b7195fbdcd9c89e
nafeeur <nafeeur@iamsheba.xyz>
2021-03-24 10:26:02 +0600

    ⬆️ Warranty Unit List from a Class
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-398

--- config/pos.php
+++ config/pos.php
@@ -2,6 +2,9 @@
 
 return [
     'warranty_unit' => [
-        'day' => ['bn' => 'দিন', 'en' => 'day'], 'week' => ['bn' => 'সপ্তাহ', 'en' => 'week'], 'month' => ['bn' => 'মাস', 'en' => 'month'], 'year' => ['bn' => 'বছর', 'en' => 'year']
+        'day' => ['bn' => 'দিন', 'en' => 'day'],
+        'week' => ['bn' => 'সপ্তাহ', 'en' => 'week'],
+        'month' => ['bn' => 'মাস', 'en' => 'month'],
+        'year' => ['bn' => 'বছর', 'en' => 'year']
     ],
 ];
