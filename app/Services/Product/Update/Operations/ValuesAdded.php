1f81307a18b46cc72745183622e9ac1e55a3b85e
shovan <shovan.sheba@gmail.com>
2021-03-24 21:59:57 +0600

    ⬆️ Product update> values add and delete
    Type(s): UPDATE
    
    Issue(s):
    - JIRA:
    https://blabol.atlassian.net/browse/MSBE-325?atlOrigin=eyJpIjoiNWU5Zjg4MDQ4NzA5NDI4YmFhNmVkM2Q1YTAzNzljN2UiLCJwIjoiaiJ9
    https://blabol.atlassian.net/browse/MSBE-326?atlOrigin=eyJpIjoiM2ZmMThjMGU5YTY5NGIyYjk4MGNmOGE2NTEyMGZjNjQiLCJwIjoiaiJ9

---
+++ app/Services/Product/Update/Operations/ValuesAdded.php
@@ -0,0 +1,13 @@
+<?php namespace App\Services\Product\Update\Operations;
+
+
+
+
+class ValuesAdded extends ValuesUpdated
+{
+    public function apply()
+    {
+        $this->operationsForValueAdd();
+        $this->resolveProductChannel();
+    }
+}
---
+++ app/Services/Product/Update/Operations/ValuesDeleted.php
@@ -0,0 +1,12 @@
+<?php namespace App\Services\Product\Update\Operations;
+
+
+class ValuesDeleted extends ValuesUpdated
+{
+    public function apply()
+    {
+        $this->deleteDiscardedCombinations();
+        $this->operationsForOldValues();
+        $this->resolveProductChannel();
+    }
+}
