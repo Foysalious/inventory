291239b50a8a8070ce19d79b449914c9cd364269
nafeeur <nafeeur@iamsheba.xyz>
2021-03-28 11:47:53 +0600

    ⬆️
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-456

--- app/Helper/Collection/CollectionHelpers.php
+++ app/Helper/Collection/CollectionHelpers.php
@@ -22,7 +22,7 @@ if( !function_exists('getCollectionDefaultThumbFolder')) {
      * @return string
      */
     function getCollectionDefaultThumbFolder() : string {
-        return config('filesystems.disks.s3.default_image_path').'/images/collections/thumbs/';
+        return config('s3.url').'images/collections/thumbs/';
     }
 }
 
@@ -52,7 +52,7 @@ if( !function_exists('getCollectionDefaultAppThumbFolder')) {
      */
 
     function getCollectionDefaultAppThumbFolder() : string {
-        return config('filesystems.disks.s3.default_image_path').'/images/collections/app_thumbs/';
+        return config('s3.url').'images/collections/app_thumbs/';
     }
 }
 
@@ -82,7 +82,7 @@ if( !function_exists('getCollectionDefaultBannerFolder')) {
      */
 
     function getCollectionDefaultBannerFolder() : string {
-        return config('filesystems.disks.s3.default_image_path').'/images/collections/banner/';
+        return config('s3.url').'images/collections/banner/';
     }
 }
 
@@ -112,7 +112,7 @@ if( !function_exists('getCollectionDefaultAppBannerFolder')) {
      */
 
     function getCollectionDefaultAppBannerFolder() : string {
-        return config('filesystems.disks.s3.default_image_path').'/images/collections/banner/';
+        return config('s3.url').'images/collections/banner/';
     }
 }
 
