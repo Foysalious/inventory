927854b4eb5ba40f17184484b6f5a88bbee2d486
nafeeur <nafeeur@iamsheba.xyz>
2021-04-01 14:06:46 +0600

    ⬆️ Product Image Updated
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-338

--- app/Services/ProductImage/Updater.php
+++ app/Services/ProductImage/Updater.php
@@ -29,6 +29,7 @@ class Updater
                 ->first()['image_link'];
 
             $this->productImageRepositoryInterface->where('product_id', $productId)->where('id', $deleteRequestedImageList[$i])->delete();
+            dd($imageLink);
             $this->deleteFileFromCDN($imageLink);
         }
     }
