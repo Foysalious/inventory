f38ee6219307eb39f0f3ba005dc359c5deb19906
shovan <shovan.sheba@gmail.com>
2021-04-12 01:25:56 +0600

    🚧 Skulist for order calculation
    Type(s): WIP
    
    Issue(s):
    - JIRA:

--- app/Interfaces/SkuRepositoryInterface.php
+++ app/Interfaces/SkuRepositoryInterface.php
@@ -4,6 +4,7 @@
 interface SkuRepositoryInterface extends BaseRepositoryInterface
 {
     public function getSkusByPartnerId($partnerId, $offset = 0, $limit = 50);
-    public function getSkusByIdsAndChannel($channelId,array $skus);
+    public function getSkusByIdsAndChannel(array $skus, $channelId);
+    public function getSkuDetails($channelId, $skuId);
 
 }
