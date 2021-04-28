b25023b8f6f87f46c9681ec7fc94eaf650e21fe6
nafeeur <nafeeur@iamsheba.xyz>
2021-04-28 10:34:28 +0600

    ⬆️ Update category thumb
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-539

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -49,12 +49,77 @@ class CategoryController extends Controller
     /**
      * @param CategoryRequest $request
      * @return JsonResponse
+     *
+     * @OA\Post(
+     *      path="/api/v1/partners/37900/categories",
+     *      operationId="createCategory",
+     *      tags={"Partners Category API"},
+     *      summary="To create a category",
+     *      description="creating partners category",
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\Schema (ref="{}")
+     *      ),
+     *      @OA\Response(
+     *          response=201,
+     *          description="Successful operation",
+     *          @OA\JsonContent(ref="")
+     *       ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      )
+     *     )
      */
     public function store($partner,CategoryRequest $request)
     {
         return $this->categoryService->create($request, $partner);
 
     }
+
+    /**
+     * @param Request $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     *
+     * @OA\Put (
+     *      path="/api/v1/partners/37900/categories/10091",
+     *      operationId="updatingcategory",
+     *      tags={"Partners Category API"},
+     *      summary="To update a category",
+     *      description="updating partner's category",
+     *      @OA\Parameter (
+     *          name="id",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *          type="integer"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\JsonContent(ref="")
+     *       ),
+     *     @OA\Response(
+     *          response=404,
+     *          description="Category not found",
+     *          @OA\JsonContent(ref="")
+     *       ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      )
+     *     )
+     */
 
     public function update($partner, $category,CategoryRequest $request)
     {
