585520d8c839591b79211b1d93a2061d28bcbe20
foysalious <foysal@iamsheba.xyz>
2021-05-19 12:38:06 +0600

    ⬆️ Product Details for webstore  partner specific
    Type(s): CREATE
    
    Issue(s):
    - https://blabol.atlassian.net/browse/MSBE-707

--- app/Http/Controllers/Webstore/ProductController.php
+++ app/Http/Controllers/Webstore/ProductController.php
@@ -23,11 +23,11 @@ class ProductController extends Controller
      *      summary="Get Category Tree List by Partner",
      *      description="",
      *      @OA\Parameter(name="partner", description="partner id", required=true, in="path", @OA\Schema(type="integer")),
+     *      @OA\Parameter(name="product", description="product id", required=true, in="path", @OA\Schema(type="integer")),
      *      @OA\Response(response=200, description="Successful operation", @OA\JsonContent(ref="")),
-     *      @OA\Response(response=404, description="message: কোন ক্যাটাগরি যোগ করা হয়নি!"),
-     *      @OA\Response(response=403, description="Forbidden")
      *     )
      * @param $partner_id
+     * @param $product_id
      * @return JsonResponse
      */
     public function getProductInformation(Request $request, $partner_id, $product_id, ProductService $productService)
