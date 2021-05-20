659f09ae91b27e72ec58e2cdf713e316b83847ce
shovan <shovan.sheba@gmail.com>
2021-05-20 11:56:28 +0600

    ⬆️ Product search
    Type(s): UPDATE
    
    Issue(s):
    - JIRA:
    https://blabol.atlassian.net/browse/MSBE-695?atlOrigin=eyJpIjoiNTEzNjk2ZTIzMjUyNDg1YjkyNWYxMDIzYzE3NGI5MTAiLCJwIjoiaiJ9

--- app/Interfaces/ProductRepositoryInterface.php
+++ app/Interfaces/ProductRepositoryInterface.php
@@ -7,5 +7,13 @@ interface ProductRepositoryInterface extends BaseRepositoryInterface
     public function getProductsByCategoryId($category_id);
     public function productChannelPrice($productId);
     public function productInformation($productId);
+
+    /**
+     * @param string $searchKey
+     * @param int $partnerId
+     * @param int $limit
+     * @param int $offset
+     * @return mixed
+     */
     public function searchProductFromWebstore(string $searchKey, int $partnerId, $limit = 10, $offset = 0);
 }
