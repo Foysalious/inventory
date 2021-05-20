e9a4d50a69b5099d85cf5da9044992b72cb5f270
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2021-05-20 14:21:59 +0600

    ⬆️ Swagger Response Example for Product List
    Type(s): UPDATE

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -26,7 +26,111 @@ class ProductController extends Controller
      *      summary="Get Products List for POS by Partner",
      *      description="",
      *      @OA\Parameter(name="partner", description="partner id", required=true, in="path", @OA\Schema(type="integer")),
-     *      @OA\Response(response=200, description="Successful operation", @OA\JsonContent(ref="")),
+     *      @OA\Response(response=200, description="Successful operation",
+     *          @OA\JsonContent(
+     *          type="object",
+     *          example={
+     *               "message": "Successful",
+     *               "data": {
+     *                   "total_items": 2,
+     *                   "total_buying_price": 1800,
+     *                   "items_with_buying_price": 2,
+     *                   "products": {{
+     *                       "id": 1000328,
+     *                       "sub_category_id": 10053,
+     *                       "name": "sdf",
+     *                       "vat_percentage": 5,
+     *                       "unit": null,
+     *                       "stock": 20,
+     *                       "app_thumb": "https://s3.ap-south-1.amazonaws.com/cdn-shebadev/images/pos/services/thumbs/1608693744_jacket.jpeg",
+     *                       "variations": {
+     *                       {
+     *                       "combination": {
+     *                       {
+     *                       "option_id": 744,
+     *                       "option_name": "size",
+     *                       "option_value_id": 1478,
+     *                       "option_value_name": "l"
+     *                       },
+     *                       {
+     *                       "option_id": 745,
+     *                       "option_name": "color",
+     *                       "option_value_id": 1479,
+     *                       "option_value_name": "green"
+     *                       }
+     *                       },
+     *                       "stock": 10,
+     *                       "channel_data": {
+     *                       {
+     *                       "sku_channel_id": 1438,
+     *                       "channel_id": 1,
+     *                       "purchase_price": 90,
+     *                       "original_price": 105,
+     *                       "discounted_price": 95,
+     *                       "discount": 10,
+     *                       "is_discount_percentage": 0,
+     *                       "wholesale_price": 105
+     *                       },
+     *                       {
+     *                       "sku_channel_id": 1439,
+     *                       "channel_id": 1,
+     *                       "purchase_price": 90,
+     *                       "original_price": 99.75,
+     *                       "discounted_price": 99.75,
+     *                       "discount": 0,
+     *                       "is_discount_percentage": 0,
+     *                       "wholesale_price": 105
+     *                       }
+     *                       }
+     *                       },
+     *                       {
+     *                       "combination": {
+     *                       {
+     *                       "option_id": 744,
+     *                       "option_name": "size",
+     *                       "option_value_id": 1480,
+     *                       "option_value_name": "s"
+     *                       },
+     *                       {
+     *                       "option_id": 745,
+     *                       "option_name": "color",
+     *                       "option_value_id": 1481,
+     *                       "option_value_name": "Black"
+     *                       }
+     *                       },
+     *                       "stock": 10,
+     *                       "channel_data": {
+     *                       {
+     *                       "sku_channel_id": 1440,
+     *                       "channel_id": 2,
+     *                       "purchase_price": 90,
+     *                       "original_price": 105,
+     *                       "discounted_price": 105,
+     *                       "discount": 0,
+     *                       "is_discount_percentage": 0,
+     *                       "wholesale_price": 105
+     *                       },
+     *                       {
+     *                       "sku_channel_id": 1441,
+     *                       "channel_id": 1,
+     *                       "purchase_price": 90,
+     *                       "original_price": 99.75,
+     *                       "discounted_price": 99.75,
+     *                       "discount": 0,
+     *                       "is_discount_percentage": 0,
+     *                       "wholesale_price": 105
+     *                       }
+     *                       }
+     *                       }
+     *                       },
+     *                       "created_at": "2021-05-16T17:57:32.000000Z"
+     *                       }
+     *                  },
+     *                  "deleted_products": {},
+     *              }
+     *           },
+     *       ),
+     *      ),
      *      @OA\Response(response=404, description="message: স্টকে কোন পণ্য নেই! প্রয়োজনীয় তথ্য দিয়ে স্টকে পণ্য যোগ করুন।"),
      *      @OA\Response(response=403, description="Forbidden")
      *     )
