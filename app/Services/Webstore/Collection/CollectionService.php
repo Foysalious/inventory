f647e42ace8e039450ac9d50c5f3f83ad62aefc7
foysalious <foysal@iamsheba.xyz>
2021-05-20 11:48:59 +0600

    ⬆️ Collection Error handle
    Type(s): UPDATE
    
    Issue(s):
    - https://blabol.atlassian.net/browse/MSBE-710

--- app/Services/Webstore/Collection/CollectionService.php
+++ app/Services/Webstore/Collection/CollectionService.php
@@ -1,9 +1,11 @@
 <?php namespace App\Services\Webstore\Collection;
+
 use App\Http\Resources\Webstore\CollectionResource;
 use App\Interfaces\CategoryRepositoryInterface;
 use App\Traits\ResponseAPI;
 use Illuminate\Http\Request;
 use App\Interfaces\CollectionRepositoryInterface;
+use function PHPUnit\Framework\isEmpty;
 
 class CollectionService
 {
@@ -16,10 +18,11 @@ class CollectionService
         $this->collectionRepositoryInterface = $collectionRepositoryInterface;
     }
 
-    public function getCollectionsByPartner($request, int $partner_id){
+    public function getCollectionsByPartner($request, int $partner_id)
+    {
         list($offset, $limit) = calculatePagination($request);
-        $resource = $this->collectionRepositoryInterface->getAllCollectionforWebstore($offset, $limit,$partner_id);
-        if(!$resource) return $this->error("Collection not found!", 404);
+        $resource = $this->collectionRepositoryInterface->getAllCollectionforWebstore($offset, $limit, $partner_id);
+        if ($resource->isEmpty()) return $this->error("Collection not found!", 404);
         $collections = CollectionResource::collection($resource);
 
         return $this->success("Successful", ['collections' => $collections], 200);
