58307a705b6a5031c913cf2951439e6d70bbd996
Foysal <foysal@iamsheba.xyz>
2021-06-03 12:06:32 +0600

    ⬆️ Categories route change
    Type(s): UPDATE
    
    Issue(s):
    - https://blabol.atlassian.net/browse/MSBE-709

--- routes/api.php
+++ routes/api.php
@@ -36,7 +36,7 @@ Route::group(['prefix'=>'v1'], function(){
     Route::group(['prefix'=>'webstore/partners'], function() {
         Route::get('{partner_id}/products/search', [WebstoreProductController::class, 'search']);
         Route::get('{partner_id}/products/{product_id}', [WebstoreProductController::class, 'show']);
-        Route::get('{partner_id}/category', [WebstoreCategoryController::class, 'getAllCategory']);
+        Route::get('{partner_id}/categories', [WebstoreCategoryController::class, 'getAllCategory']);
         Route::get('{partner_id}/collections', [WebstoreCollectionController::class, 'index']);
 
     });
