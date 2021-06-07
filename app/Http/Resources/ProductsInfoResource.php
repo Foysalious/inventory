74cde6e04a8b2fb3bd48b9595fb6de365a73c0cd
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2021-06-07 14:21:40 +0600

    🐛 Isset instead of array key exists
    Type(s): FIX

--- app/Http/Resources/ProductsInfoResource.php
+++ app/Http/Resources/ProductsInfoResource.php
@@ -39,7 +39,7 @@ class ProductsInfoResource extends JsonResource
             if (!array_key_exists('combination', $each_product['variations'])){
                 foreach ($each_product['variations'] as $each_combination){
                     $total_items++;
-                    if (array_key_exists( 'channel_data', $each_combination)){
+                    if (isset( $each_combination['channel_data'])){
                         $each_product_total_buying_cost += $each_combination['stock'] * $each_combination['channel_data'][0]['purchase_price'];
                         $items_with_buying_price++;
                     }
