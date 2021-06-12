6748ec9043b9d07aa35a55ea931986235a42ee65
Ebne Faiz Al-Amin <faiz@iamsheba.xyz>
2021-06-13 00:30:15 +0600

    ⬆️ Sku stock comment removed
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-824

--- app/Http/Resources/SkuResource.php
+++ app/Http/Resources/SkuResource.php
@@ -11,24 +11,15 @@ class SkuResource extends JsonResource
         return [
 
             'id' => $this->id,
-            'name'  => $this->name,
+            'product_name'  => $this->product->name,
             'product_id' => $this->product_id,
-            'warranty' => $this->warranty,
-            'warranty_unit' => $this->warranty_unit,
-            'sku_channel' => $this->skuChannels,
+            'warranty' => $this->product->warranty,
+            'warranty_unit' => $this->product->warranty_unit,
+            'vat_percentage' => $this->product->vat_percentage,
+            'unit' => $this->product->unit,
             'stock' => $this->stock,
-
-
-//            'id' => $this->id,
-//            'product_name'  => $this->product->name,
-//            'product_id' => $this->product_id,
-//            'warranty' => $this->product->warranty,
-//            'warranty_unit' => $this->product->warranty_unit,
-//            'vat_percentage' => $this->product->vat_percentage,
-//            'unit' => $this->product->unit,
-//            'stock' => $this->stock,
-//            'sku_channel' => $this->skuChannels,
-//            'combination' => $this->sku_details->combinations
+            'sku_channel' => $this->skuChannels,
+            'combination' => $this->sku_details->combinations
         ];
     }
 
