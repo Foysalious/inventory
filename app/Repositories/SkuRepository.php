5b542bdf1c0ad752a5cc9c5399ef525a84c41401
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2021-07-08 14:44:42 +0600

    🔥 Cost Stock
    Type(s): REMOVE

--- app/Repositories/SkuRepository.php
+++ app/Repositories/SkuRepository.php
@@ -13,9 +13,9 @@ class SkuRepository extends BaseRepository implements SkuRepositoryInterface
     public function getSkusByPartnerId($partnerId, $offset = 0, $limit = 50)
     {
 
-        return $this->model->select('skus.id', 'skus.stock', 'products.id as product_id', 'products.category_id', 'products.warranty', 'products.warranty_unit', 'products.vat_percentage')->leftJoin('products', 'skus.product_id', '=', 'products.id')
+        return $this->model->select('skus.id', 'products.id as product_id', 'products.category_id', 'products.warranty', 'products.warranty_unit', 'products.vat_percentage')->leftJoin('products', 'skus.product_id', '=', 'products.id')
             ->where('products.partner_id', $partnerId)->with(['skuChannels' => function ($q) {
-                $q->select('id', 'sku_id', 'channel_id', 'cost', 'price', 'wholesale_price');
+                $q->select('id', 'sku_id', 'channel_id', 'wholesale_price');
             }, 'product'])
             ->skip($offset)->take($limit)
             ->get();
@@ -24,7 +24,7 @@ class SkuRepository extends BaseRepository implements SkuRepositoryInterface
     public function getSkusByIdsAndChannel(array $skus,$channelId)
     {
         return $this->model->whereIn('id', $skus)->with(['skuChannels' => function ($q) use ($channelId) {
-                $q->where('channel_id',$channelId)->select('id as  sku_channel_id','sku_id','channel_id','cost','price','wholesale_price');
+                $q->where('channel_id',$channelId)->select('id as  sku_channel_id','sku_id','channel_id','wholesale_price');
             },'product'=> function($q) {
             $q->select('id','warranty','warranty_unit','vat_percentage', 'name', 'unit_id')->with('unit');
         }])->get();
