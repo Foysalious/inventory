b019c89756c756a440ed6c78979b44a56d45fa85
nafeeur <nafeeur@iamsheba.xyz>
2021-08-04 14:35:36 +0600

    ⬆️ Product Update log
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-974

--- routes/api.php
+++ routes/api.php
@@ -33,7 +33,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::group(['prefix'=>'v1'], function() {
-    //Route::group(['middleware' => 'ip.whitelist'], function () {
+    Route::group(['middleware' => 'ip.whitelist'], function () {
         Route::group(['prefix'=>'webstore'], function() {
             Route::group(['prefix'=>'partners'], function() {
                 Route::get('{partner_id}/products/search', [WebstoreProductController::class, 'search']);
@@ -77,6 +77,5 @@ Route::group(['prefix'=>'v1'], function() {
         Route::put('partners/{partner_id}/stock-update', [SkuController::class, 'updateSkuStock']);
         Route::post('partners/{partner_id}/products/{product_id}/add-stock', [SkuController::class, 'addStock']);
         Route::put('partners/{partner_id}',[DataMigrationController::class, 'updatePartnersTable']);
-    //});
-
+    });
 });
