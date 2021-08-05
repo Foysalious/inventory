28643b1222e1fc05fa693ee0d376f0cb5a4cd028
shovan <shovan.sheba@gmail.com>
2021-08-05 21:39:33 +0600

    ⬆️ Updated_after filter for category-tree
    Type(s): UPDATE
    
    Issue(s):
    - JIRA:
    https://blabol.atlassian.net/browse/MSBE-1137?atlOrigin=eyJpIjoiZjdiODVhNGNjMjMzNGI3OGFjNDlkMjkxZTY5ZGEyNzEiLCJwIjoiaiJ9

--- app/Interfaces/CategoryRepositoryInterface.php
+++ app/Interfaces/CategoryRepositoryInterface.php
@@ -4,12 +4,14 @@
 interface CategoryRepositoryInterface extends BaseRepositoryInterface
 {
 
-    public function getCategoriesByPartner($partner_id);
+    public function getCategoriesByPartner($partner_id, $updated_after);
 
     public function getCategoriesForWebstore($partner_id);
 
     public function getProductsByCategoryId($category_id);
 
     public function getSubCategoryIds(array $category_ids);
+
+    public function getDeletedCategories($partner_id,$updated_after);
 }
 
