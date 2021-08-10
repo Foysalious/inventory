184aeb1d6824367dac838bacf268b0b614150e36
shovan <shovan.sheba@gmail.com>
2021-08-10 21:29:24 +0600

    ➕ Call reward api
    Type(s): ADD
    
    Issue(s):
    - JIRA:
    https://blabol.atlassian.net/browse/MSBE-1120?atlOrigin=eyJpIjoiNGY5YjcxMmE3YzgzNGExZTk1ZTNhY2ZjMmQzYjBkNzIiLCJwIjoiaiJ9

--- config/sheba.php
+++ config/sheba.php
@@ -3,5 +3,5 @@
 return [
     'portals' => ['admin-portal', 'partner-portal', 'manager-app', 'customer-app', 'customer-portal', 'resource-portal', 'resource-app', 'bondhu-app', 'bondhu-portal', 'automatic', 'business-portal', 'digigo-portal'],
     'whitelisted_ip_redis_key_name' => env('WHITELISTED_IP_REDIS_KEY_NAME', 'WHITELISTED_IPS'),
-    'api_url' => env('SHEBA_API_URL'),
+    'api_url' => env('SHEBA_API_URL')
 ];
