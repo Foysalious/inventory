30bccb499d6933534e0054d8fbe86d660dbfeea7
Foysal <foysal@iamsheba.xyz>
2021-08-26 15:16:41 +0600

    ➕ setWebstoreChannel added for webstore channel

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -106,7 +106,7 @@ class Product extends BaseModel
     public function getOriginalPrice($channel = 2)
     {
         /** @var  $priceCalculation PriceCalculation */
-        return  app(PriceCalculation::class)->setProduct($this)->setChannel($channel)->getOriginalPrice();
+        return  app(PriceCalculation::class)->setProduct($this)->setWebstoreChannel($channel)->getOriginalPrice();
     }
 
     public function getDiscountedPrice($channel = 2)
