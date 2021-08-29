7329b42f6b084be2d6ae95879a2f2545ba43b7a5
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2021-08-29 10:25:26 +0600

    ⬆️ Add curly braces around the nested statement
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-1072

--- app/Repositories/CategoryRepository.php
+++ app/Repositories/CategoryRepository.php
@@ -25,12 +25,12 @@ class CategoryRepository extends BaseRepository implements CategoryRepositoryInt
                 ->select('categories.id', 'categories.name', 'categories.parent_id', 'categories.thumb as thumb', 'categories.is_published_for_sheba', 'category_partner.is_default');
         })->withCount('products')->where('parent_id', NULL);
 
-        if($updated_after)
+        if($updated_after) {
             $category_query = $category_query->where(function ($q) use ($updated_after) {
                 $q->where('updated_at', '>=', $updated_after);
                 $q->orWhere('created_at', '>=', $updated_after);
             });
-
+        }
         return $category_query->get();
     }
 
